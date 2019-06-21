2d76913ec58589592ade02043bfcb1b43176f80e
Rashard Pleasant <rashard.pleasant@gmail.com>
2019-06-21 03:11:01 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,11 +2,14 @@ require 'pry'
 
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
+      collection <<
     binding.pry
     yield(array[i])
     i += 1
   end
+  collection
 end
 
 binding.pry
